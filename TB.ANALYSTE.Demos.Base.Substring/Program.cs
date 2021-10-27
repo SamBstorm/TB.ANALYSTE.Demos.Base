7b65972b58d79e1c793e3a1115ca126be548d874
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-27 10:06:10 +0200

    Ajoutez des fichiers projet.

---
+++ TB.ANALYSTE.Demos.Base.Substring/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TB.ANALYSTE.Demos.Base.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!".Substring(0,5));
+            string message = "Hola! Bienvenu en C#!";
+            string matiere = message.Substring(18);
+            Console.WriteLine(matiere);
+            string input = Console.ReadLine();
+            input = input.Substring(3);
+            Console.WriteLine( input );
+        }
+    }
+}
